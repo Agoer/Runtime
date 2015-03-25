34a67d4f1ba0a05350a95d3f217cb6588b6325b2
lichan <10915819@qq.com>
2015-03-25 11:07:55 +0800

    example

---
+++ Runtime/Runtime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Runtime
+//
+//  Created by 李蝉 on 15/3/25.
+//  Copyright (c) 2015年 宜信-宜定盈. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
